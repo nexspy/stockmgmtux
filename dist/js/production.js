6a98cfda4872749bcce7a7a46dc78260ccc635f5
Nirmal Limbu <nirmalmaswa@live.com>
2021-09-16 08:42:37 +0545

    link click updated

--- dist/js/production.js
+++ dist/js/production.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 
     var $btn_nav = $('.nav_btn');
+    var $btn_link = $('.sidebar a');
 
     $btn_nav.click(function(){
         $('.mobile_nav_items').toggleClass('active');
+    });
+
+    $btn_link.click(function(e) {
+        e.preventDefault();
+
+        $btn_link.removeClass('active');
+        $(this).addClass('active');
     });
 
 });
